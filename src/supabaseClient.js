5aef50ec3b2f37436a83e3b54c3b233566a4cc3e
p451 <antoni.duhovgmail.com>
2025-04-02 13:40:58 +0300

    41

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://noaifjmpumjawayimeyo.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vYWlmam1wdW1qYXdheWltZXlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwNDUzNDUsImV4cCI6MjA1NTYyMTM0NX0.BHjvsZ1aCQgcraUp8K8UOBXLwSNTXa7Zc5pi1Ghn7jw'
+const supabaseUrl = 'https://kwgqmiwprnujqkjihllg.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3Z3FtaXdwcm51anFramlobGxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1ODMxMDMsImV4cCI6MjA1OTE1OTEwM30.JhcYebTLjYwk8udLS85gf5i_Mj2foSG5ZrrmEhRoW44'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
