b9e7779246c624202e7d8742ef331ed66140ce2a
p451 <antoni.duhovgmail.com>
2025-05-05 15:16:27 +0300

    tenant11

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-const supabaseServiceKey = process.env.REACT_APP_SUPABASE_SERVICE_ROLE_KEY;
+const supabaseUrl = 'https://kwgqmiwprnujqkjihllg.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3Z3FtaXdwcm51anFramlobGxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1ODMxMDMsImV4cCI6MjA1OTE1OTEwM30.JhcYebTLjYwk8udLS85gf5i_Mj2foSG5ZrrmEhRoW44';
+const supabaseServiceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3Z3FtaXdwcm51anFramlobGxnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MzU4MzEwMywiZXhwIjoyMDU5MTU5MTAzfQ.qke0O9tgAbgqQ9plsMsA5NWCOg8XehAY8HL0s0P45jk';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
