ba3a875dd2c4eab0eb7518ae348ab1b52c0f4be0
p451 <antoni.duhovgmail.com>
2025-05-08 15:06:12 +0300

    fix: update supabaseAnonKey to the correct value

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://kwgqmiwprnujqkjihllg.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3Z3FtaXdwcm51anFramlobGxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1ODMxMDMsImV4cCI6MjA1OTE1OTEwM30.JhcYebTLjYwk8udLS85gf5i_Mj2foSG5ZrrmEhRoW44';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3Z3FtaXdwcm51anFramlobGxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3MDU1NTEsImV4cCI6MjA2MjI4MTU1MX0.plhNaMEg8jiiNBvkMNPPbtsevM43ArGEXVe_TbVJE54';
 
 // Create a single instance
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
