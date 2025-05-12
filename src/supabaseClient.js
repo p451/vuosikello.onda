c5fe9cc27dac922c526b2b8d7041e5ee7b7d7549
p451 <antoni.duhovgmail.com>
2025-05-12 11:34:42 +0300

    refactor: remove unused invocation of create_user function from supabaseClient

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -10,11 +10,3 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     storageKey: 'vuosikello-auth'
   }
 });
-
-(async () => {
-  const { data, error } = await supabase.functions.invoke('create_user', {
-    body: { name: 'Functions' },
-  })
-  console.log('invoke data:', data)
-  console.log('invoke error:', error)
-})();
