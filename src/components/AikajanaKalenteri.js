ccb74f2aeba0c49e97dce8c2e7227d257d6b3458
p451 <antoni.duhovgmail.com>
2025-05-13 23:30:28 +0300

    fix: correct date comparison logic in isSameDay function

--- src/components/AikajanaKalenteri.js
+++ src/components/AikajanaKalenteri.js
@@ -496,7 +496,7 @@ const AikajanaKalenteri = () => {
   const isSameDay = (date1, date2) => {
     return date1.getFullYear() === date2.getFullYear() &&
            date1.getMonth() === date2.getMonth() &&
-           date1.getDate() === date1.getDate();
+           date1.getDate() === date2.getDate();
   };
 
   const parseLocalDate = (dateString) => {
